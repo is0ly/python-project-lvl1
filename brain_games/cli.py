bf06bfa40a5f21994cb9234280b7817cfbaf110c
is0ly <iljaosipov@gmail.com>
2021-01-10 23:08:06 +0300

    add linter & github actions

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,11 @@
+"""[summary]
+    """
 import prompt
 
 
 def welcome_user():
+    """[summary]
+    this is myfucntion
+    """
     name = prompt.string('May I have your name? ')
     print(f'Hello, { name }')
